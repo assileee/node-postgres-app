f3a8f72a4422c395e49e38cdd8f222aa50acf8df
Assile Zeidan <assile.zeidan@hotmail.com>
2025-02-27 17:09:44 +0100

    Put feature

--- index.js
+++ index.js
@@ -14,6 +14,19 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.put('/data/:id', (req, res) => {
+    const id = req.params.id;
+    if (dataStore[id]) {
+        dataStore[id] = req.body; // Update the data
+        res.json({
+            message: "Data updated successfully",
+            id: id,
+            newData: dataStore[id]
+        });
+    } else {
+        res.status(404).send('Data not found');
+    }
+});
 // Start the server
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
